232d710ec6b66dd581ed2e3b875325275c5b0dc5
Mohammed Rabeeh <mohammadrabeeh@gmail.com>
2021-05-01 16:10:33 +0530

    style: add eslint and prettier with husky

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-function App() {
+import React from 'react';
+
+const App = () => {
   return (
-    <div className="App">
+    <div>
       <h2>Squeely!</h2>
     </div>
   );
-}
+};
 
 export default App;
